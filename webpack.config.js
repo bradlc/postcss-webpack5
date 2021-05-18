048db0fd814ead1ed70baffeb461cbc6a8132c17
Brad Cornes <bradlc41@gmail.com>
2021-05-18 17:14:01 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const fs = require('fs')
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const postcss = require('postcss')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  cache: {
+    type: 'filesystem',
+  },
+  mode: 'production',
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  (root, result) => {
+                    let file = path.resolve(__dirname, 'example.txt')
+                    let contents = fs.readFileSync(file, 'utf8')
+
+                    root.walkAtRules('contents', (node) => {
+                      node.replaceWith(postcss.comment({ text: contents }))
+                    })
+
+                    result.messages.push({
+                      plugin: 'plugin',
+                      type: 'dependency',
+                      file,
+                      parent: result.opts.from,
+                    })
+
+                    return root
+                  },
+                ],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+}
